9ada37139a3c747ba601f1920c0344a7d9e6cc81
Timm Bremus <t.bremus@bremus-solutions.de>
2017-10-04 20:17:12 +0200

    Initial project set-up

---
+++ BSolutions.Bocons/BSolutions.Bocons.Test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BSolutions.Bocons.Test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
