42002cbf4903a46b08941e5aa0e158c0f87082c6
Timm Bremus <tbremus@bremus-solutions.de>
2018-05-01 19:48:54 +0200

    Create Tag Helper for non-interactive Dropdown items

---
+++ BSolutions.Bocons/BSolutions.Bocons/Controls/Buttons/ButtonDropdownTextTagHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace BSolutions.Bocons.Controls.Buttons
+{
+    using BSolutions.Brecons.Core.Controls;
+    using BSolutions.Brecons.Core.Extensions;
+    using Microsoft.AspNetCore.Razor.TagHelpers;
+
+    [HtmlTargetElement("text", ParentTag = "button-dropdown")]
+    public class ButtonDropdownTextTagHelper : BreconsTagHelperBase
+    {
+        protected override void RenderProcess(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "span";
+            output.AddCssClass("dropdown-item-text");
+        }
+    }
+}
