921ae573a0cbb256d3e611ccc3c738e951e41986
Timm Bremus <tbremus@bremus-solutions.de>
2018-05-05 13:32:03 +0200

    Dropdown Button now supports variations

---
+++ BSolutions.Bocons/BSolutions.Bocons/Enumerations/DropdownButtonPosition.cs
@@ -0,0 +1,19 @@
+﻿namespace BSolutions.Bocons.Enumerations
+{
+    using BSolutions.Brecons.Core.Attributes.Enumerations;
+
+    public enum DropdownButtonPosition
+    {
+        [EnumInfo("dropup")]
+        Top,
+
+        [EnumInfo("dropright")]
+        Right,
+
+        [EnumInfo("dropdown")]
+        Bottom,
+
+        [EnumInfo("dropleft")]
+        Left
+    }
+}
